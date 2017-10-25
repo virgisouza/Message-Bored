c547fd5847da1593e100bf2e4a36e4169d4e7b02
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-10-24 15:45:49 -1000

    completed routes setting on server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const db = require('./models');
-
+const methodOverride = require('method-override');
 const routes = require('./routes');
 const PORT = process.env.PORT || 3000;
 
@@ -12,12 +12,22 @@ const Topic = db.topics;
 const Message = db.messages;
 const User = db.users;
 
-app.use(express.static('public'));
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
 app.use('/api', routes);
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('./routes');
+});
+
+let users = require('./routes/users');
+app.use('/users', users);
+let topics = require('./routes/topics');
+app.use('/topics', topics);
+let messages = require('./routes/messages');
+app.use('/messages', messages);
 
 app.listen(PORT, () => {
   db.sequelize.sync({force: false});
